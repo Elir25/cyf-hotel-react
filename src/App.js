13010117ed7623c17c02ac7b348e791f24b99467
Elir25 <elirendiles@gmail.com>
2021-05-01 12:57:56 +0200

    step 2, cities cards added and link to each website

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import Bookings from "./Bookings";
 import "./App.css";
+import Card from "./Card";
 import Heading from "./Heading";
+import TouristInfoCards from "./TouristInfoCards";
 
 const App = () => {
   return (
     <div className="App">
       <Heading />
+      <TouristInfoCards />
       <Bookings />
     </div>
   );
--- src/Heading.js
+++ src/Heading.js
@@ -14,14 +14,3 @@ const Heading = () => {
 };
 
 export default Heading;
-
-/**Extract the `<header>` from the `src/App.js` 
- * file to be its own separate component called `Heading`. 
- * Make sure that you import and render the `<Heading />` 
- * component within `src/App.js`. In the `Heading` component, 
- * render the hotel's logo in an `<img>` 
- * (you can use `https://image.flaticon.com/icons/svg/139/139899.svg` 
- * or find your own image URL). You can adjust the CSS by editing `src/App.css` 
- * to make your Heading looks better if necessary.
-
-**Test:** The header should be displayed with a logo on the page. */
